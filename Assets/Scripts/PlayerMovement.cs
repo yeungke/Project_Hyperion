bf6e32ca509d86da6086f6db6fd95d84b5e9bc5e
Kevin Yeung <yeungke@sheridancollege.ca>
2022-04-09 17:58:30 -0400

    Player Animation Update
    -Player idle, run, crouch, jump and attack animations added, and linked to player object in animator
    -Sprite pivots have been altered to center the sprite within the collider
    -Player attack trigger and key have been added (default key is K)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,35 +5,57 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     private bool jump = false;
     private bool crouch = false;
 
+    [SerializeField] private KeyCode attackKey = KeyCode.K;
+
     [SerializeField] private bool canJump = false;
     [SerializeField] private bool canCrouch = false;
+    [SerializeField] private bool canAttack = false;
 
 
     void GetUserInput()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump") && canJump == true)
+        {
             jump = true;
+            animator.SetBool("IsJumping", true);
+        }
 
         if (Input.GetButtonDown("Crouch") && canCrouch == true)
             crouch = true;
         else if (Input.GetButtonUp("Crouch"))
             crouch = false;
+
+        if (Input.GetKeyDown(attackKey))
+        {
+            animator.SetTrigger("AttackTrigger");
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
     }
 
     public void SetJump(bool val)
     {
         canJump = val;
     }
-
-
 
     public void SetCrouch(bool val)
     {
