e576971a2a7e2d442766c7ccb6d4c815545a9358
JMan42069 <99372021+JMan42069@users.noreply.github.com>
2022-04-09 15:28:02 -0400

    added tmp background to tilemap

---
+++ Assets/Art/Tiled Tilemaps/Capstone_Hyperion/background_tmp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="background_tmp" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="src/background_tmp.png" width="128" height="128"/>
+</tileset>
